5998800d6c721a5e0ef51894b299562c5d69c6f4
wildfirez98 <james@areafamily.us>
2022-03-26 12:53:11 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import Gallery from './components/Gallery'
+import SearchBar from './components/SearchBar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    let [search, setSearch] = useState('')
+    let [message, setMessage] = useState('Search for Music!')
+    let [data, setData] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            document.title = `${search} Music`
+            const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
+            const resData = await response.json()
+            console.log(resData)
+        }
+        fetchData()
+    })
+
+    return (
+        <div>
+            <SearchBar />
+            {message}
+            <Gallery />
+        </div>
+    )
 }
 
-export default App;
+export default App
