9fd19cfaf17106fabfdf5ea9edf1f0a5c05b8054
wildfirez98 <james@areafamily.us>
2022-03-26 13:23:59 -0500

    App is running at basic level

--- src/App.js
+++ src/App.js
@@ -1,29 +1,44 @@
-import { useEffect, useState } from 'react'
-import Gallery from './components/Gallery'
-import SearchBar from './components/SearchBar'
+import React, { useEffect, useState } from 'react'
+import Gallery from './components/Gallery';
+import SearchBar from './components/SearchBar';
+import '../src/App.css';
 
-function App(){
-    let [search, setSearch] = useState('')
-    let [message, setMessage] = useState('Search for Music!')
-    let [data, setData] = useState([])
 
-    useEffect(() => {
-        const fetchData = async () => {
-            document.title = `${search} Music`
-            const response = await fetch('https://itunes.apple.com/search?term=the%20grateful%20dead')
-            const resData = await response.json()
-            console.log(resData)
-        }
-        fetchData()
-    })
+const App = () => {
+  let [search, setSearch] = useState('')
+  let [message, setMessage] = useState('Search for Music!')
+  let [data, setData] = useState([])
 
-    return (
-        <div>
-            <SearchBar />
-            {message}
-            <Gallery />
-        </div>
-    )
+  const API_URL = 'https://itunes.apple.com/search?term='
+
+  useEffect(() => {
+      if(search) {
+          const fetchData = async () => {
+              document.title = `${search} music`
+              const response = await fetch(API_URL + search)
+              const resData = await response.json()
+              if (resData.results.length > 0) {
+                  return setData(resData.results)
+              } else {
+                  return setMessage('Not Found.')
+              }
+          }
+          fetchData()
+      }
+  }, [search])
+
+  const handleSearch = (e, term) => {
+      e.preventDefault()
+      setSearch(term)
+  }
+
+  return (
+      <div>
+          <SearchBar handleSearch={handleSearch} />
+          {message}
+          <Gallery data={data} />
+      </div>
+  )
 }
 
 export default App
